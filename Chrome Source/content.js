0a012d1837e246771c7604151346847d1c8c2956
Euan Hollidge <ehollidg@f0r1s3.codam.nl>
2019-05-15 13:38:48 +0200

    New Colour Oicker JS

--- Chrome Source/content.js
+++ Chrome Source/content.js
@@ -12,7 +12,7 @@ if (profiles != null)
 			});
 			chrome.storage.local.get("colourP", (vValue) => {
 				var proBar = $("body > div.page > div.page-content.page-content-fluid > div > div.align-top > div > div.container-item.profile-item.full-width > div > div.user-column.flex.flex-direction-column > div._ > div.profile-left-box > div > div > div.user-progress > div > a:nth-child(1) > div > div.progress-bar");
-				proBar.css("background", vValue.colourP);
+				proBar.css("background", "#" + vValue.colourP);
 			});
 		}
 	});
